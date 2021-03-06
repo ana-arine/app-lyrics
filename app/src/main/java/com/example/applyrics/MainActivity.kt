c1ea4e64aafd453b746a9cd56b55b5c65b7d92c1
Ana Arine <analucia.arine@gmail.com>
2021-03-06 11:24:32 -0300

    Design do Splash

--- app/src/main/java/com/example/applyrics/MainActivity.kt
+++ app/src/main/java/com/example/applyrics/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.applyrics
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
+        buscarMusica()
     }
+
+    fun buscarMusica() {
+        Handler(
+            Looper.getMainLooper()).postDelayed({
+            val telaBuscarMusica =
+                Intent(this, BuscaMusicaActivity::class.java)
+            startActivity(telaBuscarMusica)
+            finish()
+        }, 3000)}
 }
