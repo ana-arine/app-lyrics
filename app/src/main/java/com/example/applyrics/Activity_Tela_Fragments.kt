cc7d42c0e0edf4b2e5393ec852748fe05a3ce45c
Gabriela Tavares <gabriela_st@live.com>
2021-03-06 12:41:15 -0300

    Layout

---
+++ app/src/main/java/com/example/applyrics/Activity_Tela_Fragments.kt
@@ -0,0 +1,11 @@
+package com.example.applyrics
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Activity_Tela_Fragments : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity__tela__fragments)
+    }
+}
