7feba52ae24d0d35c642176e5c097d6f31296fb5
Giovani Machado <giovanimachadodev@gmail.com>
2021-03-07 01:14:15 -0300

    Arrumado Classe Musica, Ativado permissão Internet

--- app/src/main/java/com/example/applyrics/Activity_Tela_Fragments.kt
+++ app/src/main/java/com/example/applyrics/Activity_Tela_Fragments.kt
@@ -14,6 +14,7 @@ import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager.widget.ViewPager
 import com.example.applyrics.model.Musica
 import com.google.android.material.tabs.TabLayout
+import kotlinx.android.synthetic.main.activity_tela_fragments.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -21,12 +22,46 @@ import retrofit2.Response
 
 class Activity_Tela_Fragments : AppCompatActivity() {
 
-    private lateinit var banda: EditText
-    private lateinit var musica : EditText
-    private lateinit var letramusica : TextView
+    class BuscarFragment : Fragment() {
+
+
+        override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+        ): View? {
+
+            return inflater.inflate(R.layout.fragment_buscar, container, false)
+        }
+
+        override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+            super.onViewCreated(view, savedInstanceState)
+
+
+            var banda: EditText = view.findViewById(R.id.edtBanda)
+            var musica: EditText = view.findViewById(R.id.edtMusica)
+            var botao: Button = view.findViewById(R.id.btn)
+
+
+            botao.setOnClickListener {
+                val nomebanda = banda.text.toString()
+                val nomemusica = musica.text.toString()
+                if(nomebanda.isNotEmpty() && nomemusica.isNotEmpty()){
+                    val bundle = Bundle()
+                    bundle.putString("BANDA", nomebanda)
+                    bundle.putString("MUSICA", nomemusica)
+                    val destino = ListaFragment()
+                    destino.arguments = bundle
+                } else {
+                    banda.error = "Errrrrrou!"
+                }
+            }
+
+        }
+
+    }
 
     class LetraFragment : Fragment() {
-        private lateinit var letra : TextView
+        private lateinit var letramusica : TextView
         override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
@@ -38,7 +73,45 @@ class Activity_Tela_Fragments : AppCompatActivity() {
         override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
             super.onViewCreated(view, savedInstanceState)
 
-            letra = view.findViewById(R.id.txtLetra)
+//                val bandarecebida = arguments?.getString("BANDA").toString()
+//                val musicarecebida = arguments?.getString("MUSICA").toString()
+                val bandarecebida = "Coldplay"
+                val musicarecebida = "Yellow"
+
+
+                buscarMusica(bandarecebida, musicarecebida)
+                letramusica = view.findViewById(R.id.txtLetra)
+
+
+        }
+
+        fun buscarMusica(nomebanda: String, nomemusica: String){
+            val retrofitClient = Network.RetrofitConfig("https://api.lyrics.ovh/v1/")
+
+            val servico = retrofitClient.create(MusicaService::class.java)
+
+            val chamada = servico.buscarMusica(nomebanda,nomemusica)
+
+            chamada.enqueue(
+                object : Callback<Musica> {
+                    //Comunicação com a API OK!
+                    override fun onResponse(call: Call<Musica>, response: Response<Musica>) {
+                        val letra = response.body()?.letra
+                        letra?.let {
+                            if (it.isNotEmpty()) {
+                                letramusica.text = it
+                            } else {
+                                letramusica.error = "Opa... Não tem Letra!"
+                            }
+                        }
+                    }
+                    override fun onFailure(call: Call<Musica>, t: Throwable) {
+                        letramusica.text = "Opa... Houve erro na solicitação." +
+                                "\nTente novamente mais tarde."
+                    }
+                }
+            )
+
 
         }
     }
@@ -76,32 +149,6 @@ class Activity_Tela_Fragments : AppCompatActivity() {
         }
     }
 
-    class BuscarFragment : Fragment() {
-
-        private lateinit var banda: EditText
-        private lateinit var musica : EditText
-
-        override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
-        ): View? {
-
-            var vista: View
-            vista = inflater.inflate(R.layout.fragment_buscar, container, false)
-            var botao = vista.findViewById<Button>(R.id.btn)
-            botao.setOnClickListener (View.OnClickListener { 
-
-            })
-            return vista
-        }
-
-        override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-            super.onViewCreated(view, savedInstanceState)
-
-
-        }
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tela_fragments)
@@ -112,50 +159,8 @@ class Activity_Tela_Fragments : AppCompatActivity() {
 
         viewPager.adapter = PagerAdapter(supportFragmentManager)
         tabLayout.setupWithViewPager(viewPager)
-
-        var botao: Button? = findViewById(R.id.btn)
-        botao?.setOnClickListener {
-            val nomebanda = banda.text.toString()
-            val nomemusica = musica.text.toString()
-            if(nomebanda.isNotEmpty() && nomemusica.isNotEmpty()){
-                buscarMusica(nomebanda, nomemusica)
-            } else {
-                banda.error = "Errrrrrou!"
-            }
-        }
-
     }
 
-    fun buscarMusica(nomebanda: String, nomemusica: String){
-        //Iniciar requisição a API para buscar CEP
-        val retrofitClient = Network.RetrofitConfig("https://api.lyrics.ovh/v1/")
-
-
-        val servico = retrofitClient.create(MusicaService::class.java)
-
-        val chamada = servico.buscarMusica(nomebanda,nomemusica)
-
-        chamada.enqueue(
-            object : Callback<Musica> {
-                //Comunicação com a API OK!
-                override fun onResponse(call: Call<Musica>, response: Response<Musica>) {
-                    val letrateste = response.body()?.letra
-                    letrateste?.let {
-                        if (it.isNotEmpty()) {
-                            letramusica.text = it
-                        } else {
-                            letramusica.error = "Opa... Não tem Letra!"
-                        }
-                    }
-                }
-                override fun onFailure(call: Call<Musica>, t: Throwable) {
-                    letramusica.text = "Opa... Houve erro na solicitação." +
-                            "\nTente novamente mais tarde."
-                }
-            }
-        )
-
-
-
-    }
 }
+
+
--- app/src/main/java/com/example/applyrics/model/Musica.kt
+++ app/src/main/java/com/example/applyrics/model/Musica.kt
@@ -3,7 +3,7 @@ package com.example.applyrics.model
 import com.google.gson.annotations.SerializedName
 
 class Musica(
-    @SerializedName("nomeArtista")val nomeArtista: String,
-    @SerializedName("nomeMusica")val nomeMusica: String,
-    @SerializedName("letra")val letra: String,
+    val nomeArtista: String,
+    val nomeMusica: String,
+    @SerializedName("lyrics")val letra: String,
 )
